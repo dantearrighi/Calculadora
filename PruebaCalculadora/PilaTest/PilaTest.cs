ea30778571c658d5fe318a8402401685417a86be
Dante <dantea@fibertel.com.ar>
2015-09-04 21:19:10 -0300

    PilaTest: Rompe sacar
    Agregada clases y metodos para probar pilas.

---
+++ PruebaCalculadora/PilaTest/PilaTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PruebaCalculadora;
+
+namespace PilaTest
+{
+    [TestClass]
+    public class PilaTest
+    {
+        
+        
+        
+        [TestMethod]
+        public void ponerTest()
+        {
+            Pila p = new Pila();    
+            p.poner("Primera");
+            p.poner("Segunda");
+            p.poner("Tercera");
+            Assert.AreEqual(3, p.size());
+
+        }
+
+        [TestMethod]
+        public void sacarTest()
+        {
+            Pila p = new Pila();
+            p.poner("Primera");
+            p.poner("Segunda");
+            p.poner("Tercera");
+            string rta = p.sacar();
+            Assert.AreEqual("Tercera", rta);
+        }
+
+  
+    }
+}
---
+++ PruebaCalculadora/PruebaCalculadora/Pila.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCalculadora
+{
+    public class Pila
+    {
+       public  void poner(string cadena)
+        {
+            pila.Push(cadena);
+
+        }
+
+        Stack<string> pila = new Stack<string>();
+
+        public string sacar()
+        {
+            return pila.ElementAt(size());
+        }
+
+
+      public   int size()
+        {
+            return pila.Count();
+        }
+    }
+}
